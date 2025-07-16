36e820d05625bb8674e5d00bc833138acd78a358
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-16 12:54:34 +0530

    Calculate percentage.java

---
+++ Calculate percentage.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Calculate the Percentage of Marks");
+        Scanner sc=new Scanner(System.in);
+        
+        System.out.println("Enter the Subect1 Mark: ");
+        int m1=sc.nextInt();
+        
+        System.out.println("Enter the Subect2 Mark: ");
+        int m2=sc.nextInt();
+        
+        System.out.println("Enter the Subect3 Mark: ");
+        int m3=sc.nextInt();
+        
+        System.out.println("Enter the Subect4 Mark: ");
+        int m4=sc.nextInt();
+        
+        System.out.println("Enter the Subect5 Mark: ");
+        int m5=sc.nextInt();
+        
+        int total=m1+m2+m3+m4+m5;
+        System.out.println("Total- "+total);
+        double per=(total/500.0)*100;
+        System.out.println("The Percentage Of the Student Mark is : "+per+"%");
+        
+    }
+}
+
