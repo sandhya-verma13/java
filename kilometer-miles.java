3133f6a853bf74cfb899f567fcbb2b2a5ff560b8
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-17 11:02:57 +0530

    kilometer-miles.java

---
+++ kilometer-miles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the KiloMeter : ");
+        int k=sc.nextInt();
+        double m=k*0.621371;
+        System.out.println("Miles : "+m);
+    }
+}
