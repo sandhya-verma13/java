ce86b83454111863c33be6b4e01f85aaef536010
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-17 15:55:20 +0530

    comparission operator.java

---
+++ comparission operator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Enter the Number: ");
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a>8);
+        
+    }
+}
