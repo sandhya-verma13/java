21a70241f5243bac7ec20934d0084bb13dc38cc0
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-18 17:03:23 +0530

    detect2&3space.java

---
+++ detect2&3space.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        String word="My name  is   harry";
+        System.out.println(word.indexOf("  "));
+        System.out.println(word.indexOf("   "));
+        
+    }
+}
