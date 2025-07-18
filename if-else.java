97bb0bd9ae1aed5a8663ec51cfd69563aba3960e
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-18 17:08:38 +0530

    if-else.java

---
+++ if-else.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int a;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        if(a==11){
+            System.out.println("I am 11");
+        }else{
+            System.out.println("I am not 11");
+        }
+        
+    }
+}
