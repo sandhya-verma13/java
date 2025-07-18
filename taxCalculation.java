cb66ab2a60c29d0ab08eabaff2b3c1de9abb738f
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-18 17:45:54 +0530

    taxCalculation.java

---
+++ taxCalculation.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        float tax=0;
+        float income;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the income in lakhs: ");
+        income=sc.nextFloat();
+        if(income<2.5){
+            tax=tax+0;
+        }
+        else if(income>=2.5f && income<=5f){
+            tax=tax + 0.05f*(income - 2.5f);
+        }
+        else if(income>5f && income<=10.0f){
+            tax=tax + 0.05f *(5.0f- 2.5f);
+            tax=tax + 0.2f *(income- 5f);
+        }
+        else if(income>10.0f){
+            tax=tax + 0.05f *(5f- 2.5f);
+            tax=tax + 0.2f *(10.5f- 5f);
+            tax=tax + 0.3f *(income- 10.0f);
+        }
+        System.out.println("The total taz is : "+ tax);
+        
+    }
+}
