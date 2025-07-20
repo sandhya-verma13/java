078cbf476612ff28297cb9305b71a9b7569013d5
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-20 20:48:21 +0530

    pattern1UsingForLoop.java

---
+++ pattern1UsingForLoop.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the Row: ");
+        n=sc.nextInt();
+        for(int i=n;i>0;i--){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
