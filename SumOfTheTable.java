6ffa7c97d8232ab1a31dbacfcc16c3f9a3c71e87
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-20 20:54:20 +0530

    SumOfTheTable.java

---
+++ SumOfTheTable.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int n,sum=0;
+        System.out.print("Enter the Table: ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(int i=10;i>=1;i--){
+            System.out.printf("%d X %d = %d\n",n,i,n*i);
+            sum+=n*i;
+        }
+        System.out.printf("Sum of the Table %d is :%d ",n,sum);
+    }
+}
