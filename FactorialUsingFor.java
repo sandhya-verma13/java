798e87de5fbf2bccc6a4b2d3f181cce628c0b2fa
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-20 20:51:56 +0530

    FactorialUsingFor.java

---
+++ FactorialUsingFor.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int n,fact=1;
+        System.out.print("Enter the Number: ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            fact*=i;
+        }
+        System.out.printf("The Factorial of %d is : %d",n,fact);
+    }
+}
