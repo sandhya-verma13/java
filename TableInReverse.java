9123f3358a0b70244cc7559537aedb7f50ee4272
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-20 20:51:17 +0530

    TableInReverse.java

---
+++ TableInReverse.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        int n;
+        System.out.print("Enter the Table: ");
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        for(int i=10;i>=1;i--){
+            System.out.printf("%d X %d = %d\n",n,i,n*i);
+        }
+    }
+}
