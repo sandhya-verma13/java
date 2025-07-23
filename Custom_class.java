6aabdb05366e66948010285a5aa111dc9e086678
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-23 21:00:48 +0530

    OOps

---
+++ Custom_class.java
@@ -0,0 +1,16 @@
+class Employee{
+    int id;
+    String name;
+}
+public class Custom_class {
+
+    public static void main(String[] args) {
+        System.out.println("The Custom Class");
+        Employee sandhya= new Employee();
+        sandhya.id=101;
+        sandhya.name="Verma";
+        System.out.println(sandhya.id);
+        System.out.println(sandhya.name);
+
+    }
+}
