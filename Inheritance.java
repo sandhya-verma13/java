76bc7a6eae850fec922f661ddf4373c018c47c81
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-07-30 20:39:48 +0530

    Inheritance

---
+++ Inheritance.java
@@ -0,0 +1,18 @@
+class Animal{
+    
+    Animal(){
+        System.out.println("Welcome to Animal World !!..");
+    }
+
+}
+class Dog extends Animal{
+    Dog(){
+        System.out.println("I am a Dog and i am barking.........");
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog d=new Dog();
+
+    }
+}
