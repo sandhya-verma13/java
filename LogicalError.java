8fad6a310ea6161b4dfc6439678d6255a8954433
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-08-18 21:18:11 +0530

    Error Demos

---
+++ LogicalError.java
@@ -0,0 +1,10 @@
+public class LogicalError {
+    public static void main(String[] args) {
+        int a=0 //Shows syntax error
+        b=1; //b not declared
+        for(int i=0;i<5;i++){
+            System.out.println(2*i+1);//Logical error
+        }
+
+    }
+}
