0399ffc54fb285553b0a4c0e2b92ed3afddc0e23
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2025-08-25 20:20:32 +0530

    logical error

--- LogicalError.java
+++ LogicalError.java
@@ -1,7 +1,7 @@
 public class LogicalError {
     public static void main(String[] args) {
-        int a=0 //Shows syntax error
-        b=1; //b not declared
+        //int a=0 //Shows syntax error
+        //b=1; //b not declared
         for(int i=0;i<5;i++){
             System.out.println(2*i+1);//Logical error
         }
